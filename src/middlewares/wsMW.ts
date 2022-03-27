50c1b54defd38269379cec63fabe662163804fd1
AymerickCotche <aymerick.cotche@hotmail.fr>
2022-03-27 11:33:16 +0200

    fix some bug

--- src/middlewares/wsMW.ts
+++ src/middlewares/wsMW.ts
@@ -7,10 +7,12 @@ declare global {
   }
 }
 
+let socket: any;
+
 const wsMW: Middleware = (store) => (next) => (action) => {
   switch (action.type) {
     case WS_CONNECT: {
-      const socket = window.io('http://192.168.1.19:3001/');
+      socket = window.io('http://192.168.1.19:3001/');
 
       socket.on('server_message', (message: { text:string, id: number, author: string, date: string }) => {
         store.dispatch(sendMessage(message));
@@ -18,7 +20,6 @@ const wsMW: Middleware = (store) => (next) => (action) => {
       break;
     }
     case WS_SEND_TO_SERVER: {
-      const socket = window.io('http://192.168.1.19:3001/');
       socket.emit('client_message', { text: store.getState().text, author: store.getState().user.pseudo, date: new Date().toLocaleString() });
       break;
     }
